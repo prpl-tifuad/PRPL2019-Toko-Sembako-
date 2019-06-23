92bf5473626ae8f5d77769c45c987903110ffd26
dapid suhendra <40122475+dapidsuhendra@users.noreply.github.com>
2019-06-23 19:28:50 +0700

    Add files via upload

---
+++ Toko Sembako (Update)/index.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+  	<nav class="navbar navbar-expand-lg navbar-light bg-dark">
+		<a class="navbar-brand text-white" style="font-size: 30px;" href="#">Toko Sembako</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+			<div class="navbar-nav">
+				<a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+				<a class="nav-item nav-link" href="#">Features</a>
+			</div>
+		</div>
+	</nav>
+  	<?php 
+		require 'connect.php';
+		$sql = 'SELECT * FROM product';
+		$result = mysqli_query($con, $sql);
+	?>
+  	<section>
+  		<div class="container">
+  			<div class="row">
+  				<div class="col">
+  					<?php while($product = mysqli_fetch_object($result)) { ?>
+					<div class="card" style="width: 18rem;">
+						<img src="images/default.jpg" class="card-img-top" alt="...">
+						<div class="card-body">
+							<h5 class="card-title"><?php echo $product->name; ?></h5>
+							<p class="card-text">Kode Barang :<?php echo $product->id; ?></p>
+							<p class="card-text">Harga : <?php echo $product->price; ?></p>
+							<a class="btn btn-info" href="cart.php?id= <?php echo $product->id; ?> &action=add">Pesan</a>
+						</div>
+					</div>
+					<?php } ?>
+  				</div>
+  			</div>
+  		</div>
+  	</section>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
